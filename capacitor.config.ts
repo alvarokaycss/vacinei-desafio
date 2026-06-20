fe83755ea007db10919170d3d0e15c5525a862bf
Álvaro Kayc <155195361+alvarokaycss@users.noreply.github.com>
2026-06-20 13:55:43 -0300

    chore: commit inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vacinei',
+  webDir: 'www'
+};
+
+export default config;
